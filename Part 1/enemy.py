fcc35166b15e33e569672aeb1a826d16a7e23172
Brayden MacEachern <144717636+BananasandCheeese@users.noreply.github.com>
2025-03-18 13:27:14 -0700

    enemy rotating

--- Part 1/enemy.py
+++ Part 1/enemy.py
@@ -1,22 +1,49 @@
 import pygame as pg
 from pygame.math import Vector2
+import math
 
 class Enemy(pg.sprite.Sprite):
     def __init__(self, waypoints, image):
         self.waypoints = waypoints
         self.pos = Vector2(self.waypoints[0])
         self.target_waypoint = 1
+        self.speed = 2
+        self.angle = 0
         pg.sprite.Sprite.__init__(self)
-        self.image = image
+        self.original_image = image
+        self.image = pg.transfrom.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
 
     def update(self):
         self.move()
+        self.rotate()
 
     def move(self):
         #define a target waypoint
-        self.target = Vector2(self.waypoints[self.target_waypoint])
-        self.movement = self.target - self.pos
-        self.pos += self.movement.normalize()
-        self.rect.center = self.pos
+        if self.target_waypoint < len(self.waypoints):
+            self.target = Vector2(self.waypoints[self.target_waypoint])
+            self.movement = self.target - self.pos
+        else:
+            #enemy has reached the end of the path
+            self.kill()
+
+        #calculate distance to target
+        dist = self.movement.length()
+        #check if remaining distance is greater than enemy the speed
+        if dist >= self.speed:
+            self.pos += self.movement.normalize() * self.speed
+        else:
+            if dist != 0:
+                self.pos += self.movement.normalize() * dist
+            self.target_waypoint += 1
+
+def rotate(self):
+    #calculate distance to next waypoint
+    dist = self.target - self.pos
+    #use distance to calculate angle
+    self.angle = math.degrees(math.atan2(-dist[1], dist[2]))
+    #rotate the image and update rectangle
+    self.image = pg.transfrom.rotate(self.original_image, self.angle)
+    self.rect = self.image.get_rect()
+    self.rect.center = self.pos
