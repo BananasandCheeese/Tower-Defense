6c5ee9128613164e32971d620def28457ec7e60d
Brayden MacEachern <144717636+BananasandCheeese@users.noreply.github.com>
2025-03-20 13:26:43 -0700

    new waypoints

--- Part 1/main.py
+++ Part 1/main.py
@@ -16,26 +16,23 @@ pg.display.set_caption("Tower Defense")
 
 # Load images
 #map
-map_image = pg.image.load('levels/level.png').convert_alpha()
+map_image = pg.image.load('Part 1/assets/levels/level.png').convert_alpha()
 #enemies
 enemy_image = pg.image.load('Part 1/assets/images/enemies/enemy_1.png').convert_alpha()
 
+#load json data for level
+with open('Part 1/assets/levels/level.tmj') as file:
+  world_data = json.load(file)
 
 
 #create world
-world = World(map_image)
+world = World(world_data, map_image)
+world.process_data()
 
 # Create groups
 enemy_group = pg.sprite.Group()
 
-waypoints = [
-  (100, 100),
-  (400, 200),
-  (400, 100),
-  (200, 300),
-]
-
-enemy = Enemy(waypoints, enemy_image)
+enemy = Enemy(world.waypoints, enemy_image)
 enemy_group.add(enemy)
 
 # Game loop
@@ -49,7 +46,7 @@ while run:
   world.draw(screen)
 
   # Draw enemy path
-  pg.draw.lines(screen, "grey0", False, waypoints)
+  pg.draw.lines(screen, "grey0", False, world.waypoints)
 
   # Update groups
   enemy_group.update()
